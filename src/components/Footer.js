aa752d5ae9ab1e5498214e886d4fb646c5c71278
Rulesstack <65716510+Rulesstack@users.noreply.github.com>
2020-05-26 16:16:23 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,14 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      
     </h2>
     <br />
     <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © {new Date().getFullYear()} Rulesstack .All rights reserved. 
         </span>
       </div>
     </footer>
